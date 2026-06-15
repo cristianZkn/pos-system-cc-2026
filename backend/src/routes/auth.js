dcf11818e4ad02580794ba0bc62b258c8b8e9514
Cristian Soto <cristian.soto5@virginiogomez.cl>
2026-06-15 19:14:30 -0400

    Update rateLimit

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -13,12 +13,13 @@ const loginValidations = [
 
 // Limitador exclusivo para intentos fallidos de inicio de sesión
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 5, // Límite de 5 peticiones fallidas por IP en la ventana de tiempo
-  skipSuccessfulRequests: true, // ¡Clave! No cuenta si el login es exitoso
-  message: { error: 'Demasiados intentos fallidos de inicio de sesión. Por favor, inténtelo de nuevo después de 15 minutos.' }
+  windowMs: 5 * 60 * 1000, // 5 minutos de bloqueo
+  max: 10, // Límite de 10 peticiones fallidas por IP en la ventana de tiempo
+  skipSuccessfulRequests: true, //No cuenta si el login es exitoso
+  message: { error: 'Demasiados intentos fallidos de inicio de sesión. Por favor, inténtelo de nuevo después de 5 minutos.' }
 });
 
+// Aplicamos el limitador
 router.post('/login', loginLimiter, loginValidations, validate, login);
 router.post('/logout', logout);
 router.get('/me', authMiddleware, me);
